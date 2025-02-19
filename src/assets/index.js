d3dd804afdf863854605435a1b58b2e611bce179
kalki <kalki@kali.kalki>
2025-02-19 16:41:09 +0530

    only footer left

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,53 +1,22 @@
-import airbnb from "./airbnb.png";
-import bill from "./bill.png";
-import binance from "./binance.png";
-import card from "./card.png";
-import coinbase from "./coinbase.png";
-import dropbox from "./dropbox.png";
-import logo from "./logo.svg";
-import quotes from "./quotes.svg";
-import robot from "./robot.png";
-import send from "./Send.svg";
-import shield from "./Shield.svg";
-import star from "./Star.svg";
+import Image1 from './Image1.jpg';
+import Image2 from './Image2.jpg';
+import Image3 from './Image3.jpg';
+import Image4 from './Image4.jpg';
+import Image5 from './Image5.jpg';
+import outdoor from './outdoor.jpg'
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
-import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
-import facebook from "./facebook.svg";
-import instagram from "./instagram.svg";
-import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
-
-export {
-  airbnb,
-  bill,
-  binance,
-  card,
-  coinbase,
-  dropbox,
-  logo,
-  quotes,
-  robot,
-  send,
-  shield,
-  star,
+import indoorSec from "./indoorSec.jpg";
+import aisec from "./aisec.jpeg";
+export { 
+  Image1, 
+  Image2, 
+  Image3, 
+  Image4, 
+  Image5,
   menu,
   close,
-  google,
-  apple,
-  arrowUp,
-  discount,
-  facebook,
-  instagram,
-  linkedin,
-  twitter,
-  people01,
-  people02,
-  people03,
+  outdoor,
+  indoorSec,
+  aisec
 };
